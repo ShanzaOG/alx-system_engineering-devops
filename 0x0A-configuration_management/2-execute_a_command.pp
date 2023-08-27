eef8bf52c1a65dc793d8ea9937b91808c877d178
Allan <allanshanza@gmail.com>
2023-08-27 07:06:37 -0700

    correction

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-# Kills a process
-exec { 'kill_process_killmenow':
-  command => 'pkill -f "killmenow"',
-  onlyif  => 'pgrep -f "killmenow"',
-}
+# command that kils a process
 
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+}
