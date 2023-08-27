b0de28b55e92f2596994c02b5cc5d96d23ea027c
Allan <allanshanza@gmail.com>
2023-08-27 07:19:31 -0700

    second commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Changes SSH config file using puppet
+exec { 'ssh_config_update':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
+
