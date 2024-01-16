3ab597510c14b8e6d329cb30cfaa8369693ad8b7
ShanzaOG <allanshanza@gmail.com>
2024-01-16 10:15:42 -0800

    trial

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,23 +14,13 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'Google Chrome Version 120.0.6099.225'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
+    results = response.json()
 
-    # Check for a successful request (status code 200)
-    if response.status_code == 200:
-        try:
-            # Parse the JSON response
-            results = response.json()
-            return results.get('data').get('subscribers')
-        except (KeyError, ValueError) as e:
-            print(f"Error parsing JSON: {e}")
-            return 0
-    elif response.status_code == 404:
-        print(f"Subreddit '{subreddit}' not found.")
-        return 0
-    else:
-        print(f"Error: {response.status_code}")
-        return 0
+    try:
+        return results.get('data').get('subscribers')
 
+    except Exception:
+        return 0
