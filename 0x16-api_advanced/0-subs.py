e1f8c17d29754664b69d33de2e5f9d2c810fba34
ShanzaOG <allanshanza@gmail.com>
2024-01-17 13:26:22 -0800

    error correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,12 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 17 11:47:53 2020
-
 @author: Shanza Allan
 """
 
-
-from json import loads
 from requests import get
 
 
@@ -15,8 +12,6 @@ def number_of_subscribers(subreddit):
     """
     function that queries the Reddit and returns the number
     of subscribers for a given subreddit.
-
-    If no results  the function should return 0.
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
@@ -24,7 +19,6 @@ def number_of_subscribers(subreddit):
         'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) '
         'Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-
 
     response = get(url, headers=headers)
     
